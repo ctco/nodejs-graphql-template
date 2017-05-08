8535eb62c6b48aac0485d8b13a3244b555517a49
trioletas <olegs.gabrusjonoks@gmail.com>
2017-05-09 00:04:12 +0300

    Added GraphQL document references / message on error

--- src/routes/graphql.ts
+++ src/routes/graphql.ts
@@ -1,7 +1,13 @@
 import * as graphqlHTTP from 'koa-graphql';
 import schema from '../graphql/schema';
+import logger from '../logger';
 
 export default graphqlHTTP({
   schema,
-  graphiql: process.env.GRAPHIQL
+  graphiql: process.env.GRAPHIQL,
+  formatError: error => {
+    const {message, locations, path} = error;
+    logger.error(`GraphQL error`, {message, locations, path});
+    return error;
+  },
 });
