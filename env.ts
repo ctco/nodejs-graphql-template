49340f025ce63f561d9985c5413033b167f68bcd
trioletas <olegs.gabrusjonoks@gmail.com>
2017-05-08 20:45:17 +0300

    App startup reporting in development mode (CRA-like)

--- env.ts
+++ env.ts
@@ -1,11 +1,12 @@
 import * as envalid from 'envalid';
 
-const {url, bool} = envalid;
+const { url, bool, str } = envalid;
 
 const env = envalid.cleanEnv(process.env, {
   JOKE_SERVICE_URI: url({default: 'https://api.icndb.com'}),
   GRAPHIQL: bool({devDefault: true}),
   CORS: bool({devDefault: false}),
+  NODE_ENV: str({devDefault: 'development'})
 });
 
 export default env;
--- server.ts
+++ server.ts
@@ -1,6 +1,23 @@
 import env from './env';
 process.env = env;
+import * as chalk from 'chalk';
+
 import app from './src/app';
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`Koa application listening on port ${port}`));
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV === 'development') {
+    if (process.env.GRAPHIQL) {
+      console.log(`The GraphiQL App is running at:`);
+      console.log();
+      console.log(`  ${chalk.cyan(`http://localhost:${port}/api/graphql`)}`);
+    } else {
+      console.log(`The Koa App is running at:`);
+      console.log();
+      console.log(`  ${chalk.cyan(`http://localhost:${port}`)}`);
+    }
+  } else {
+    console.log(`The Koa App is running`);
+  }
+});
