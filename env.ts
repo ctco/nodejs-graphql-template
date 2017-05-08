3244105112dc81e7f92e95c8e89446e6761d925d
trioletas <olegs.gabrusjonoks@gmail.com>
2017-05-08 20:18:47 +0300

    default joke service URL

--- env.ts
+++ env.ts
@@ -3,7 +3,7 @@ import * as envalid from 'envalid';
 const {url, bool} = envalid;
 
 const env = envalid.cleanEnv(process.env, {
-  JOKE_SERVICE_URI: url(),
+  JOKE_SERVICE_URI: url({default: 'https://api.icndb.com'}),
   GRAPHIQL: bool({devDefault: true}),
   CORS: bool({devDefault: false}),
 });
