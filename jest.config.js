76e00daae6d44c4e8946577b22ee918d47bf0451
Oleg Gabrusenok <olegs.gabrusjonoks@gmail.com>
2017-05-14 14:25:54 +0300

    Closes #8 junit compatible test report output (#9)
    * Added junit-jest dependency
    
    * Coverage exporter
    
    * yarnignores

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "moduleFileExtensions": [
+    "ts",
+    "js"
+  ],
+  "transform": {
+    "\\.(ts)$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+  },
+  "testRegex": "/__tests__/.*\\.(ts)$"
+};
