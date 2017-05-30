a163027e64157b63b90b77087c3db1f37fdfbf4b
Eldar Aliyev <eldar.aliyev8@gmail.com>
2017-05-30 20:10:16 +0300

    Add npm run test:all:ci

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
-module.exports = Object.assign({}, require('./jest.default.config'), {
-  "testPathIgnorePatterns": [
-    "/src/graphql/__tests__"
-  ]
-});
+module.exports = Object.assign(
+  {},
+  require('./tools/jest.default.config.js'),
+  process.env.TEST_CI && require('./tools/jest.ci.default.config.js'),
+  {
+    "testPathIgnorePatterns": [
+      "/src/graphql/__tests__"
+    ]
+  }
+);
--- src/graphql/jest.config.js
+++ src/graphql/jest.config.js
@@ -1,5 +1,10 @@
-module.exports = Object.assign({}, require('../../jest.default.config'), {
-  "setupFiles": [
-    "./__tests__/__setup__/env-test.ts"
-  ],
-});
+module.exports = Object.assign(
+  {},
+  require('../../tools/jest.default.config.js'),
+  process.env.TEST_CI && require('../../tools/jest.ci.default.config.js'),
+  {
+    "setupFiles": [
+      "./__tests__/__setup__/env-test.ts"
+    ]
+  }
+);
