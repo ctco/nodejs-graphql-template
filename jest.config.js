c2b3f2cb0d96de4296275e080107ad3973ecf44b
aeldar <eldar.aliyev8@gmail.com>
2017-05-31 14:55:19 +0300

    Get rid of short circut evaluation

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = Object.assign(
   {},
   require('./jest/jest.default.config.js'),
-  process.env.CI && require('./jest/jest.ci.default.config.js'),
+  process.env.CI ? require('./jest/jest.ci.default.config.js') : {},
   {
     "testPathIgnorePatterns": [
       "/src/graphql/__tests__"
--- src/graphql/jest.config.js
+++ src/graphql/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = Object.assign(
   {},
   require('../../jest/jest.default.config.js'),
-  process.env.CI && require('../../jest/jest.ci.default.config.js'),
+  process.env.CI ? require('../../jest/jest.ci.default.config.js') : {},
   {
     "setupFiles": [
       "./__tests__/__setup__/env-test.ts"
