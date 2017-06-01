405d92a4d9f72a6a72e256d320dde4dca60b038b
Oleg Gabrusenok <olegs.gabrusjonoks@gmail.com>
2017-06-01 14:40:06 +0300

    Update env.ts

--- env.ts
+++ env.ts
@@ -5,7 +5,7 @@ const { url, bool, str } = envalid;
 const env = envalid.cleanEnv(process.env, {
   JOKE_SERVICE_URI: url({default: 'https://api.icndb.com'}),
   GRAPHIQL: bool({devDefault: true}),
-  CORS: bool({devDefault: false}),
+  CORS: bool({devDefault: true}),
   NODE_ENV: str({devDefault: 'development'}),
   LOG_LEVEL: str({default: 'info'}),
 });
