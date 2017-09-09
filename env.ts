3347571709938501d11e575af367be69f8964604
Oleg Gabrusenok <olegs.gabrusjonoks@gmail.com>
2017-09-09 13:09:40 +0300

    Issue #75 voyager (#114)
    * closes #75
    
    * externalized path configuration
    
    * fixed paths
    
    * fixed paths

--- env.ts
+++ env.ts
@@ -5,6 +5,7 @@ const { url, bool, str } = envalid;
 const env = envalid.cleanEnv(process.env, {
   JOKE_SERVICE_URI: url({default: 'https://api.icndb.com'}),
   GRAPHIQL: bool({default: true}),
+  VOYAGER: bool({default: true}),
   CORS: bool({devDefault: true, default: false}),
   NODE_ENV: str({devDefault: 'development'}),
   LOG_LEVEL: str({default: 'info'}),
