20c7f7be2d603e254f583d5c0945b42781bd005b
jelisejev <687830+jelisejev@users.noreply.github.com>
2018-04-27 14:10:29 +0300

    refactor export statement

--- e2e/client.ts
+++ e2e/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import gql from 'graphql-tag';
+export { default as gql } from 'graphql-tag';
 
 import 'isomorphic-fetch';
 
@@ -15,6 +15,3 @@ const client = () => new ApolloClient({
 });
 
 export default client;
-export {
-  gql,
-};
